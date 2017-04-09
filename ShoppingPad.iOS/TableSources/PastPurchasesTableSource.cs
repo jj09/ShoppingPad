b91ea1de41b5ed37e265f7151983ac7d91b42d1c
Jakub Jedryszek <jjed@microsoft.com>
2017-04-08 21:02:01 -0700

    code formatting

--- ShoppingPad.iOS/TableSources/PastPurchasesTableSource.cs
+++ ShoppingPad.iOS/TableSources/PastPurchasesTableSource.cs
@@ -11,23 +11,23 @@ namespace ShoppingPad.iOS
 {
     public class PastPurchasesTableSource : UITableViewSource
     {
-        PastPurchasesViewModel ViewModel;
+        private PastPurchasesViewModel _viewModel;
 
         public PastPurchasesTableSource()
         {
-            ViewModel = ServiceRegistrar.Container.Resolve<PastPurchasesViewModel>();
+            _viewModel = ServiceRegistrar.Container.Resolve<PastPurchasesViewModel>();
         }
 
         public override nint RowsInSection(UITableView tableview, nint section)
         {
-            return ViewModel.Items.Count;
+            return _viewModel.Items.Count;
         }
 
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
         {
             var cell = (PastPurchasesTableViewCell)tableView.DequeueReusableCell(PastPurchasesTableViewCell.Key);
 
-            var item = ViewModel.Items.ElementAt(indexPath.Row);
+            var item = _viewModel.Items.ElementAt(indexPath.Row);
             var isOnShoppingList = ServiceRegistrar.Container.Resolve<IShoppingService>().Items.FirstOrDefault(x => x.Title == item.Title) != null;
 
             cell.UpdateCell(item, isOnShoppingList);
@@ -37,7 +37,7 @@ namespace ShoppingPad.iOS
 
         public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
         {
-            ViewModel.CopyItemToShoppingList(ViewModel.Items.ElementAt(indexPath.Row));
+            _viewModel.CopyItemToShoppingList(_viewModel.Items.ElementAt(indexPath.Row));
             tableView.ReloadRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
             tableView.DeselectRow(indexPath, true);
         }
